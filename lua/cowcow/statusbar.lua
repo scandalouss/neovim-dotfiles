2fd6c60ec15fac3827f1179e7ade5f91759616ec
scandalouss <dking3211@gmail.com>
2023-12-19 09:28:52 -0600

    wooheeheehaahaa

--- lua/cowcow/statusbar.lua
+++ lua/cowcow/statusbar.lua
@@ -3,6 +3,11 @@ local utils = require('lualine.utils.utils')
 local godtheme=require('lualine.themes.auto') -- i dont think i have to do this
 
 --colors
+--using highlight groups to define the theme,
+--sorta like auto already does but with my own colors
+--it's so i can copy the config around to multiple pc's
+--and not have the color theme be completely booty
+
 local colors={
     black='#000000',
     white='#ffffff',
@@ -15,10 +20,6 @@ local colors={
     command=utils.extract_color_from_hllist('fg',{'Identifier'}),
 }
 
---using highlight groups to define the theme,
---sorta like auto already does but with my own colors
---it's so i can copy the config around to multiple pc's
---and not have the color theme be completely booty
 godtheme={
     normal={
         a={bg=colors.normal,fg=colors.black,gui='bold'},
