a6c58c15752cfd19b13b946578919205922dddbd
scandalouss <dking3211@gmail.com>
2024-01-08 16:47:58 -0600

    goddamnmit 2

--- lua/cowcow/init.lua
+++ lua/cowcow/init.lua
@@ -7,9 +7,6 @@ require("cowcow.anim")
 require("cowcow.treesitter")
 require("cowcow.autopairs")
 require("cowcow.statusbar")
-<<<<<<< HEAD
 require("cowcow.discordrpc")
-=======
 require("cowcow.debugger")
->>>>>>> 62277da5542acc095b58f359041992072e5bc707
 require("cowcow.remap")
